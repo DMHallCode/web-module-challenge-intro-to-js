86698339f31dfd52799eea15030c1ae9215c6708
DMHallCode <david.hall.code@gmail.com>
2023-03-11 10:27:41 -0500

    dog years

--- index.js
+++ index.js
@@ -92,9 +92,9 @@ Do the following:
  3. Return the newly calculated age
 */
 
-function dogYears(/*add your code here*/){
-  /*add your code here*/
-}
+function dogYears(age){
+  return age * 7;
+}console.log(dogYears(3));
 
 
 
