2a8269906bc6e2e869d7b19b3f07aede0854de6c
DMHallCode <david.hall.code@gmail.com>
2023-03-17 09:33:18 -0400

    completed MVP

--- index.js
+++ index.js
@@ -186,11 +186,29 @@ Use the game function below to do the following:
 
 RULES OF THE GAME: Scissors beats Paper | Paper beats Rock | Rock beats Scissors | Or there's a tie
 */
+let computer = Math.random();
 
-function game(user, computer){
-  /*add your code here*/
+if(computer <= 0.34){
+  computer = 'rock';
+}else if(computer <= 0.67){
+  computer = 'paper'
+}else if(computer > 0.67){
+  computer = 'scissors'
 }
-
+function game(user, computer){
+  if(user === computer){
+    return `it's a tie`;
+  }else if(user === 'rock' && computer === 'scissors'){
+    return `you win!`
+  }else if(user === 'paper' && computer === 'rock'){
+    return `you win!`
+  }else if(user === 'scissors' && computer === 'paper'){
+    return `you win!`
+  }else{
+    return `you lose!`
+  }
+}
+console.log(game('rock', computer));
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -204,11 +222,10 @@ Using the miles function below do the following:
 3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
-  /*add your code here*/
+function miles(km){
+  return km * 0.621371;
 }
-
-
+console.log(miles(5))
 
 //Task 5b - Centimeters to Feet
 /*
@@ -218,11 +235,10 @@ Using the feet function below do the following:
 3. Return number of feet
 */
 
-function feet(/*add your code here*/){
-  /*add your code here*/
+function feet(cm){
+  return cm / 30.48;
 }
-
-
+console.log(feet(10))
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
@@ -238,10 +254,13 @@ Using the annoyingSong function below do the following:
 4. Each time the annoyingSong is run from this loop, it should console.log the string that was returned. 
 */
 
-function annoyingSong(/*add your code here*/){
-      /*add your code here*/
+function annoyingSong(number){
+  for(let i = number; i > 0; i--){
+    return `${i} bottles of soda on the wall, ${i} bottles of soda, take one down pass
+    it around ${i -1} bottles of soda on the wall.`
+  }
 }
-
+console.log(annoyingSong(10));
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
@@ -258,10 +277,20 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(/*Your Code here */){
-/*Your Code here */
+function grade(score){
+  if(score >= 90){
+    return `you got an A`;
+  }else if(score < 90 && score >= 80){
+    return  `you got a B`;
+  }else if(score < 80 && score >= 70){
+    return `you got a C`;
+  }else if(score < 70 && score >= 60){
+    return `you got a D`;
+  }else if(score < 60){
+    return `you got an F`;
+  }
 }
-
+console.log(grade(86));
 
 
 /*💪💪💪💪💪💪💪💪💪💪 Stretch 💪💪💪💪💪💪💪💪💪💪*/
